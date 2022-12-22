5336d54da6efc8ee6acc163a6e68f124586b655e
Gupta-Saurabh0 <97726837+Gupta-Saurabh0@users.noreply.github.com>
2022-12-22 20:51:17 +0530

    Add files via upload

---
+++ src/assets/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+export default function App() {
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo list</h1>
+          <br/>
+        </div>
+      </div>
+    </>
+  );
+}
